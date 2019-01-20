f4ba66f5748bec530aac121486310c7ae37d37eb
Stefan Rimaila <stefan@rimaila.fi>
2019-01-20 08:45:13 +0200

    feat(assets): Add base color palettes

---
+++ src/assets/palettes/index.js
@@ -0,0 +1,29 @@
+// @ts-check
+import { color } from 'd3-color';
+
+const dawnbringer = [
+  '#000000',
+  '#55415f',
+  '#646964',
+  '#d77355',
+  '#508cd7',
+  '#64b964',
+  '#e6c86e',
+  '#dcf5ff',
+].map(color);
+
+const ammo8 = [
+  '#040c06',
+  '#112318',
+  '#1e3a29',
+  '#305d42',
+  '#4d8061',
+  '#89a257',
+  '#bedc7f',
+  '#eeffcc',
+].map(color);
+
+export default {
+  dawnbringer,
+  ammo8,
+};
