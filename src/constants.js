c4295d45798c2bd5036534ad3959b02ff78ead83
Stefan Rimaila <stefan@rimaila.fi>
2019-01-21 09:55:28 +0200

    feat: Add constants file

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+/**
+ * Configuration settings
+ */
+export const Config = Object.freeze({
+  localStorageKey: 'picksel',
+});
+
+/**
+ * Icon name constants
+ */
+export const IconType = Object.freeze({
+  CROP: 'crop_free',
+  EDIT: 'edit',
+  NAVIGATE_BEFORE: 'navigate_before',
+  NAVIGATE_NEXT: 'navigate_next',
+});
